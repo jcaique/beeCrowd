b7779ba9723f94b72ec3dfe7168f28bc6695985a
jcaique <jose.silva448@fatec.sp.gov.br>
2026-05-27 23:13:33 -0300

    organizing by lang

---
+++ py/1024crypto.py
@@ -0,0 +1,23 @@
+#lines = int(input())
+#counter = 1
+
+def FirstPass(AValue):
+  pass
+
+# reverse the AValue
+def SecondPass(AValue):
+  result = ''
+  i = len(AValue) - 1
+  while (i >= 0):
+    result += AValue[i]
+    i -= 1    
+  return result
+
+def ThirdPass(AValue):
+  pass
+
+#while counter <= lines:
+ # counter += 1
+  #LString = input()
+
+
